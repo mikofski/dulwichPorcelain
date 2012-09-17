e35be52305c33354eff24c7bba6bd863d8b08dc6
Mark Mikofski <bwanamarko@yahoo.com>
2012-09-16 17:10:46 -0700

    use getopts
    * only allow '.' repository
    * add verbose to display files being written

--- checkout.py
+++ checkout.py
@@ -1,27 +1,31 @@
 from dulwich.repo import Repo
 from dulwich.file import ensure_dir_exists, GitFile
 import os
-import sys
 
 
-def checkout(repo_path='.', co_ref='HEAD'):
+def checkout(args):
     """
-        Checkout a reference from a Git repository
-        :param repo_path: <str> path of repository
-        :param co_ref: <str> name of checkout reference
-        :return: None
+        Checkout a reference from a Git repository.
+        usage: dulwich checkout [--verbose] [REF]
     """
-    repo = Repo(repo_path)
+    opts, args = getopt(args, "", ["verbose"])
+    opts = dict(opts)
+    if len(args) > 0:
+        ref = args.pop(0)
+    else:
+        ref = 'HEAD'
+    # TODO: error out if unstaged or uncommited files
+    repo = Repo('.')
     obj_sto = repo.object_store
-    tree_id = repo['HEAD'].tree
-
+    tree_id = repo[ref].tree
     for entry in obj_sto.iter_tree_contents(tree_id):
         entry_in_path = entry.in_path(repo.path)
         path = os.path.split(entry_in_path.path)
         ensure_dir_exists(path[0])
-        print 'creating %s in %s' % path
+        if opts["verbose"]
+            print 'creating %s in %s' % path
         path = os.path.join(*path)
         git_file = GitFile(path, 'wb')
-        git_file.write(repo[entry.sha].data)
+        git_file.write(repo[entry_in_path.sha].data)
         git_file.close()
         os.chmod(path, entry_in_path.mode)
