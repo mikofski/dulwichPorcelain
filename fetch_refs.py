0c8601b4b7e9de06e7e18c8ac7fc8862233fd617
Mark Mikofski <bwanamarko@yahoo.com>
2012-11-09 22:46:32 -0800

    change fetch_ref from script to function

--- fetch_refs.py
+++ fetch_refs.py
@@ -3,14 +3,7 @@
     from dulwich.client import get_transport_and_path
     from dulwich.file import ensure_dir_exists, GitFile
     
-    LOCALPATH = 'c:\\path\\to\\repo'
-    REMOTE = 'git@github.com:user/path'
-    GITDIR = '.git'
     DULWICH_REFS = 'dulwich_refs'
-    MYBRANCH = 'mybranch'
-    
-    repo = Repo.init(LOCALPATH, mkdir=True)
-    client, host_path = get_transport_and_path(REMOTE)
     
     # Help on method fetch in module dulwich.client:
     # fetch(self, path, target, determine_wants=None, progress=None) unbound
@@ -22,15 +15,27 @@
     #   :param progress: Optional progress function
     #   :return: remote refs as dictionary
     
-    refs = client.fetch(host_path, r, objsto.determine_wants_all, sys.stdout.write)
-    
-    # lets write these to a file, but we can fetch them again any time
-    with open(os.path.join(repo.path, GITDIR, DULWICH_REFS), 'wb') as file:
+def fetch_refs(remote, local='.', branch='master'):
+    """
+    """
+    client, host_path = get_transport_and_path(remote)
+    # TODO: check that repo exists, if not use Repo.init(local,mkdir=True)
+    r = Repo(local)
+    gitdir = r.controldir()
+    objsto = r.object_store
+    headtree = r['HEAD'].tree
+    #treeiter = objsto.iter_tree_contents(headtree)
+    determine_wants = objsto.determine_wants_all
+    remote_refs = client.fetch(host_path, r, determine_wants,
+                              sys.stdout.write)
+
+    dulwich_refs = os.path.join(local, gitdir, DULWICH_REFS)
+    with open(dulwich_refs, 'wb') as file:
         writer = csv.writer(file, delimiter=' ')
-        for key, value in refs.items():
+        for key, value in remote_refs.items():
             writer.writerow([key, value])
 
-    myref = refs[MYBRANCH]
+    myref = refs[branch]
     tree_id = repo[myref].tree
     #iterate over tree content, giving path and blob sha.
     for entry in repo.object_store.iter_tree_contents(tree_id):
