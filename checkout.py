addcc492eb861583a4b3a4b5c46d6efcfc52a55a
Mark Mikofski <bwana.marko@yahoo.com>
2013-09-24 14:52:02 -0700

    use build index from tree

--- checkout.py
+++ checkout.py
@@ -1,6 +1,5 @@
 from dulwich.repo import Repo
-from dulwich.file import ensure_dir_exists, GitFile
-import os
+from dulwich.index import build_index_from_tree
 
 
 def checkout(repo_path='.', co_ref='HEAD'):
@@ -8,22 +7,14 @@ def checkout(repo_path='.', co_ref='HEAD'):
     Checkout a reference from a Git repository
     :param repo_path: <str> path of repository
     :param co_ref: <str> name of checkout reference
-    :return entries: <TreeEntry> named tuples
+    :return entries: <TreeEntry> list of named tuples
     """
-    # TODO: try using index.build_index_from_tree
+    # TODO: add all checkout options
     repo = Repo(repo_path)
+    indexfile = repo.index_path()
     obj_sto = repo.object_store
-    # TODO: catch not a reference
+    # TODO: catch if not a reference
     tree_id = repo[co_ref].tree
     # TODO: error out if unstaged or uncommited files
-    entries = []
-    for entry in obj_sto.iter_tree_contents(tree_id):
-        entry_in_path = entry.in_path(repo.path)
-        path = os.path.split(entry_in_path.path)
-        ensure_dir_exists(path[0])
-        path = os.path.join(*path)
-        with open(path, 'wb') as GitFile:
-            GitFile.write(repo[entry_in_path.sha].data)
-        os.chmod(path, entry_in_path.mode)
-        entries.append(entry)
-    return entries
+    build_index_from_tree(repo_path,indexfile,obj_sto,tree_id)
+    return [obj_sto.iter_tree_contents(tree_id)]
